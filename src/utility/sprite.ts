4adbddbb741a9a3a923fdd2bd9f665376a916102
Jakub Neruda <nerudaj@email.cz>
2022-04-24 10:37:19 +0200

    Factor out Sprite class

--- src/utility/sprite.ts
+++ src/utility/sprite.ts
@@ -4,14 +4,21 @@ export class Sprite {
     constructor(private img: CanvasImageSource, private clipX:number, private clipY:number, private clipW:number, private clipH:number) {}
 
     setRotation(degrees: number) {
-        this.rotation = degrees;
+        this.rotation = degrees + 90;
     }
 
     draw(canvas2d: CanvasRenderingContext2D, x: number, y:number, w: number, h: number) {
         //canvas2d.save();
         //canvas2d.rotate(rotation * Math.PI/180);
         // TODO: rotation
-        canvas2d.drawImage(this.img, this.clipX, this.clipY, this.clipW, this.clipH, x, y, w, h);
+        //canvas2d.drawImage(this.img, this.clipX, this.clipY, this.clipW, this.clipH, x, y, w, h);
         //canvas2d.restore();
+
+        canvas2d.save();
+        canvas2d.translate(x + w / 2, y + h / 2);
+        canvas2d.rotate(this.rotation * Math.PI/180.0);
+        canvas2d.translate(- x - w / 2, - y - h / 2);
+        canvas2d.drawImage(this.img, this.clipX, this.clipY, this.clipW, this.clipH, x, y, w, h);
+        canvas2d.restore();
     }
 }
