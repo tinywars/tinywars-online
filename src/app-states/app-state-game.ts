a23eaaf9558cb7e80c76bab86a99b69c95752ce2
Jakub Neruda <nerudaj@email.cz>
2022-04-30 09:59:16 +0200

    Feature: Simple AI (#23)
    * Add FastArray<T>
    
    * indev
    
    * Update fast-array.ts
    
    * Add docs
    
    * Shooting works, but different sprites dont
    
    * Fixed vector toAngle formula
    
    * Update view-player.ts
    
    * Update view-projectile.ts
    
    * Add files via upload
    
    * Update view-projectile.ts
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Cleanup
    
    * Ton of small refactors, improvements, etc
    
    * Fix lint
    
    * Apply suggestions from code review
    
    * Simple  AI
    
    * This is becoming gloriously incomprehensible gameplay
    
    * Update vector.ts

--- src/app-states/app-state-game.ts
+++ src/app-states/app-state-game.ts
@@ -2,18 +2,35 @@ import { App } from "./app";
 import { AppState } from "./app-state";
 import { Vector } from "../utility/vector";
 import { Controller } from "../utility/controller";
+import { KeyboardController } from "../utility/keyboard-controller";
 import { FastArray } from "../utility/fast-array";
 import { EventQueue } from "../events/event-queue";
 import { GameContext } from "../game/game-context";
+import { KeyCode } from "../game/key-codes";
 import { Player } from "../game/player";
 import { Projectile } from "../game/projectile";
+import { AiBrain } from "../ai/ai-brain";
+import { AiPoweredController } from "../ai/ai-controller";
 
 export class AppStateGame implements AppState {
     private gameContext: GameContext;
-    private uniqueId = 0;
+    private uniqueId = 10; /// offseting this number so values below are reserved for players
+    private controllers: Controller[] = [];
+    private aiBrains: AiBrain[] = [];
 
-    constructor(private app: App, private controllers: Controller[]) {
+    constructor(private app: App, keyboardState: Record<string, boolean>) {
         console.log("AppStateGame construction");
+
+        const ACTIVE_PLAYERS = 4;
+
+        this.controllers.push(this.createPhysicalController(0, keyboardState));
+        //this.controllers.push(this.createPhysicalController(1, keyboardState));
+
+        for (let i = 1; i < ACTIVE_PLAYERS; i++) {
+            const aiController = new AiPoweredController()
+            this.controllers.push(aiController);
+            this.aiBrains.push(new AiBrain(aiController, i));
+        }
 
         this.gameContext = {
             SCREEN_WIDTH: 1280,
@@ -25,9 +42,9 @@ export class AppStateGame implements AppState {
             
             PROJECTILE_SPEED: 256,
             PROJECTILE_DAMAGE: 1,
-            PROJECTILE_ENABLE_TELEPORT: true,
+            PROJECTILE_ENABLE_TELEPORT: false,
 
-            players: new FastArray<Player>(4, (i) => new Player(this.controllers[i])),
+            players: new FastArray<Player>(4, (i) => new Player(i, this.controllers[i])),
             projectiles: new FastArray<Projectile>(64, () => new Projectile(this.uniqueId++) ),
             eventQueue: new EventQueue(),
             
@@ -43,7 +60,6 @@ export class AppStateGame implements AppState {
         const PLAYER_INITIAL_ENERGY = 2;
         const PLAYER_MAX_ENERGY = 4;
 
-        const ACTIVE_PLAYERS = 2;
         for (let i = 0; i < ACTIVE_PLAYERS; i++)
             this.gameContext.players.grow();
 
@@ -60,6 +76,7 @@ export class AppStateGame implements AppState {
     }
 
     updateLogic(dt: number): void {
+        this.aiBrains.forEach((b) => b.update(dt, this.gameContext));
 
         this.gameContext.players.forEach((p) => p.update(dt, this.gameContext));
         this.gameContext.projectiles.forEach((p) => p.update(dt, this.gameContext));
@@ -69,5 +86,32 @@ export class AppStateGame implements AppState {
 
     getContext(): GameContext {
         return this.gameContext;
+    }
+
+    private createPhysicalController(index: number, keyboardState: Record<string, boolean>): KeyboardController {
+        const bindings = [
+            [
+                { key: "KeyW", code: KeyCode.Up },
+                { key: "KeyA", code: KeyCode.Left },
+                { key: "KeyS", code: KeyCode.Down },
+                { key: "KeyD", code: KeyCode.Right },
+                { key: "KeyE", code: KeyCode.Shoot },
+                { key: "KeyR", code: KeyCode.Boost },
+            ],
+            [
+                { key: "KeyI", code: KeyCode.Up },
+                { key: "KeyJ", code: KeyCode.Left },
+                { key: "KeyK", code: KeyCode.Down },
+                { key: "KeyL", code: KeyCode.Right },
+                { key: "KeyO", code: KeyCode.Shoot },
+                { key: "KeyP", code: KeyCode.Boost },
+            ],
+        ];
+
+        const result = new KeyboardController(keyboardState);
+        bindings[index].forEach((binding) => {
+            result.bindKey(binding.key, binding.code);
+        });
+        return result;
     }
 }
