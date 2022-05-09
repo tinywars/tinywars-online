4589c457e5c204afef453e1a8ce5aa58c8fe1f8e
Jakub Neruda <nerudaj@email.cz>
2022-05-10 00:07:25 +0200

    Replace Math.random() with calls to new PRNG class

---
+++ src/utility/prng.ts
@@ -0,0 +1,32 @@
+export class PRNG {
+    private static lastValue = 0;
+    static readonly MOD = 2 ** 32;
+
+    static setSeed(seed: number) {
+        PRNG.lastValue = seed % PRNG.MOD;
+    }
+
+    /**
+     * @returns Random integer from 0 to 2^32-1
+     */
+    static randomInt(): number {
+        PRNG.lastValue = (69069 * PRNG.lastValue + 1) % PRNG.MOD;
+        return PRNG.lastValue;
+    }
+
+    /**
+     * @returns Random float from interval <0,1)
+     */
+    static randomFloat(): number {
+        return PRNG.randomInt() / PRNG.MOD;
+    }
+
+    /**
+     * Get random integer from range <min..max)
+     * @param min Minimum value of random number (inclusive)
+     * @param max Maximum value of random number (exclusive)
+     */
+    static randomRangedInt(min: number, max: number): number {
+        return (PRNG.randomInt() % (max - min)) + min;
+    }
+}
