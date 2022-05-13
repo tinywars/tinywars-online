3379859dec7cce08a2fdb0732a64781c76267124
Jakub Neruda <nerudaj@email.cz>
2022-05-13 17:17:56 +0200

    Improve code coverage

--- src/game/game-settings.ts
+++ src/game/game-settings.ts
@@ -29,3 +29,36 @@ export interface GameSettings {
     OBSTACLE_HIT_DAMAGE: number;
     OBSTACLE_MASS: number;
 }
+
+export function getDefaultSettings(): GameSettings {
+    return {
+        SCREEN_WIDTH: 1000,
+        SCREEN_HEIGHT: 1000,
+        ANIMATION_FPS: 2,
+        TIME_TILL_RESTART: 5,
+        PLAYER_NAMES: ["red", "green", "blue", "yellow"],
+        DISPLAY_PLAYER_NAMES: true,
+        // Spawn settings
+        PLAYER_COUNT: 4,
+        NPC_COUNT: 0,
+        ROCK_COUNT: 4,
+        PLAYER_INITIAL_HEALTH: 3,
+        PLAYER_INITIAL_ENERGY: 2,
+        PLAYER_MAX_ENERGY: 4,
+        // Simulation settings
+        //   Player
+        PLAYER_FORWARD_SPEED: 500,
+        PLAYER_ROTATION_SPEED: 250,
+        PLAYER_ENERGY_RECHARGE_SPEED: 0.5,
+        PLAYER_MASS: 10,
+        //   Projectile
+        PROJECTILE_SPEED: 1000,
+        PROJECTILE_DAMAGE: 1,
+        PROJECTILE_ENABLE_TELEPORT: false,
+        PROJECTILE_MASS: 1.5,
+        //   Obstacle
+        OBSTACLE_MAX_SPEED: 750,
+        OBSTACLE_HIT_DAMAGE: 10,
+        OBSTACLE_MASS: 15,
+    };
+}
---
+++ src/game/player.spec.ts
@@ -0,0 +1,143 @@
+import { expect } from "chai";
+import { AiPoweredController } from "../ai/ai-controller";
+import { EventQueue } from "../events/event-queue";
+import { AnimationEngine, AnimationFrame } from "../utility/animation";
+import { Vector } from "../utility/vector";
+import { Player } from "./player";
+import { KeyCode } from "./key-codes";
+import { GameContext } from "./game-context";
+import { GameSettings, getDefaultSettings } from "./game-settings";
+import { FastArray } from "../utility/fast-array";
+import { Projectile } from "./projectile";
+import { Obstacle } from "./obstacle";
+
+const playerAnimationMock = {
+    idle: [new AnimationFrame(1, 2, 3, 4)],
+    hit: [new AnimationFrame(5, 6, 7, 8)],
+};
+
+const projectileAnimationMock = {
+    idle: [new AnimationFrame(9, 10, 11, 12)],
+};
+
+const obstacleAnimationMock = {
+    idle0: [new AnimationFrame(13, 14, 15, 16)],
+    idle1: [new AnimationFrame(17, 18, 19, 20)],
+    wreck0: [new AnimationFrame(21, 22, 23, 24)],
+    wreck1: [new AnimationFrame(25, 26, 27, 28)],
+    wreck2: [new AnimationFrame(29, 30, 31, 32)],
+    wreck3: [new AnimationFrame(33, 34, 35, 36)],
+};
+
+describe("Player", () => {
+    let controller: AiPoweredController;
+    let animationEngine: AnimationEngine;
+    let eventQueue: EventQueue;
+    let player: Player;
+    let settings: GameSettings;
+    let gameContext: GameContext;
+
+    beforeEach(() => {
+        controller = new AiPoweredController();
+        controller.releaseKey(KeyCode.Up);
+        controller.releaseKey(KeyCode.Down);
+        controller.releaseKey(KeyCode.Left);
+        controller.releaseKey(KeyCode.Right);
+        controller.releaseKey(KeyCode.Shoot);
+        controller.releaseKey(KeyCode.Boost);
+
+        animationEngine = new AnimationEngine(playerAnimationMock, 2);
+        eventQueue = new EventQueue();
+
+        settings = getDefaultSettings();
+
+        gameContext = {
+            settings: settings,
+            players: new FastArray<Player>(
+                1,
+                (index) =>
+                    new Player(index, controller, animationEngine, eventQueue),
+            ),
+            projectiles: new FastArray<Projectile>(
+                64,
+                (index) =>
+                    new Projectile(
+                        index,
+                        new AnimationEngine(projectileAnimationMock, 2),
+                    ),
+            ),
+            obstacles: new FastArray<Obstacle>(
+                8,
+                (index) =>
+                    new Obstacle(
+                        index,
+                        new AnimationEngine(obstacleAnimationMock, 2),
+                    ),
+            ),
+            eventQueue: eventQueue,
+            log: (msg: string) => {
+                console.log(msg);
+            },
+        };
+
+        gameContext.players.grow();
+        player = gameContext.players.getItem(0);
+    });
+
+    it("#spawn", () => {
+        player.spawn({
+            position: new Vector(42, 69),
+            initialHealth: 1,
+            initialEnergy: 2,
+            maxEnergy: 3,
+        });
+
+        expect(player.getCoords().position.x).to.equal(42);
+        expect(player.getCoords().position.y).to.equal(69);
+        expect(player.getCoords().angle).to.equal(0);
+        expect(player.getForward().x).to.equal(0);
+        expect(player.getForward().y).to.equal(0);
+        expect(player.getHealth()).to.equal(1);
+        expect(player.getEnergy()).to.equal(2);
+        expect(player.getCoords().frame.x).to.equal(1);
+        expect(player.getCoords().frame.y).to.equal(2);
+    });
+
+    it("#despawn", () => {
+        player.spawn({
+            position: new Vector(42, 69),
+            initialHealth: 1,
+            initialEnergy: 2,
+            maxEnergy: 3,
+        });
+
+        player.despawn();
+
+        const outOfView = Vector.outOfView();
+        expect(player.getCoords().position.x).to.equal(outOfView.x);
+        expect(player.getCoords().position.y).to.equal(outOfView.y);
+    });
+
+    describe("#update", () => {
+        describe("Shooting", () => {
+            it("Shoots when key is pressed and has enough energy", () => {
+                player.spawn({
+                    position: new Vector(42, 69),
+                    initialHealth: 1,
+                    initialEnergy: 2,
+                    maxEnergy: 3,
+                });
+
+                controller.pressKey(KeyCode.Shoot);
+                player.update(0, gameContext);
+
+                expect(controller.isKeyPressed(KeyCode.Shoot)).to.be.false;
+                expect(player.getEnergy()).to.equal(1);
+                expect(gameContext.eventQueue.events.length).to.equal(1);
+                expect(gameContext.eventQueue.events[0].name).to.equal(
+                    "SpawnProjectileEvent",
+                );
+            });
+        });
+    });
+});
