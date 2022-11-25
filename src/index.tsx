4315e06b4c393be2d52b1b4ddec5b1e64e097a4b
Jakub Neruda <nerudaj@email.cz>
2022-11-25 16:09:16 +0100

    Added custom font
    Font had to be converted in https://transfonter.org/ in order to work.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { Router } from "@gh0st-work/solid-js-router";
 import { render } from "solid-js/web";
 
+import "../assets/font/pixel-art-font.css";
 import "./index.css";
 import { AppTopLevel } from "./ui/v2/App";
 
